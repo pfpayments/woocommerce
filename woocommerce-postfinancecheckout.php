139f180c6a8749eaa20d1c51f64f7155350ac939
root <root@runner-n4w1exa-project-16870242-concurrent-0pxj6m>
2022-10-27 19:08:22 +0200

    Release 2.1.0

--- woocommerce-postfinancecheckout.php
+++ woocommerce-postfinancecheckout.php
@@ -3,13 +3,13 @@
  * Plugin Name: PostFinance Checkout
  * Plugin URI: https://wordpress.org/plugins/woo-postfinancecheckout
  * Description: Process WooCommerce payments with PostFinance Checkout.
- * Version: 2.0.9
+ * Version: 2.1.0
  * License: Apache2
  * License URI: http://www.apache.org/licenses/LICENSE-2.0
  * Author: wallee AG
  * Author URI: https://www.wallee.com
  * Requires at least: 4.7
- * Tested up to: 6.0.2
+ * Tested up to: 6.0.3
  * WC requires at least: 3.0.0
  * WC tested up to: 6.7.0
  *
@@ -45,7 +45,7 @@ if ( ! class_exists( 'WooCommerce_PostFinanceCheckout' ) ) {
 		 *
 		 * @var string
 		 */
-		private $version = '2.0.9';
+		private $version = '2.1.0';
 
 		/**
 		 * The single instance of the class.
